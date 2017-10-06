5f5d5548d44b394e9112c00d8cb00145690cfd07
marcossuzuki <marcossuzuki@gmail.com>
2017-10-06 11:01:47 -0300

    Fix primo

--- lab3/ex04.c
+++ lab3/ex04.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-unsigned int sumdigits (unsigned int n) {
-    if (n<10) return n;
-    return sumdigits(n/10)+n%10;
+unsigned int primo (unsigned int n, unsigned int divisor) {
+    if (divisor==1) return 1;
+    else if (n==divisor) return primo(n, divisor - 1);
+    else if (n%divisor==0) return 0;
+    return primo(n, divisor - 1);
 }
 
-unsigned int sumdigitsTR (unsigned int n, unsigned int base) {
+unsigned int primoTR (unsigned int n, unsigned int base) {
     if (n<10) return base+n;
-    return sumdigitsTR(n/10, base+n%10);
+    return primoTR(n/10, base+n%10);
 }
 
 int main(){
 
     unsigned int n; 
 
-    printf("=== Soma dos digitos ===\n");
+    printf("=== Primo identificador ===\n");
     printf("Digite n: ");
     scanf("%d", &n);
 
 
-    printf("Soma dos digitos recusrisva caudal: %d\n", sumdigitsTR(n,0));    
+    //printf("Soma dos digitos recusrisva caudal: %d\n", sumdigitsTR(n,0));    
 
-    printf("Soma dos digitos recusrisva: %d\n", sumdigits(n));  
-
+    printf("Primo recusrisva: ");  
+    if (primo(n,n-1)) printf("Eh primo!\n");
+    else printf("Nao eh primo!\n");
 
     return 0;
 }
