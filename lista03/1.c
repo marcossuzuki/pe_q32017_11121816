df456c9e4c9e778e23e21121696d7c67ab2c7979
marcossuzuki <marcossuzuki@gmail.com>
2017-10-27 04:18:22 -0200

    Exercicio 1

--- lista03/1.c
+++ lista03/1.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int ehvogal (char * entrada)
+{
+    if (*entrada=='A' || *entrada=='a') return 1;
+    if (*entrada=='E' || *entrada=='e') return 1;
+    if (*entrada=='I' || *entrada=='i') return 1;
+    if (*entrada=='O' || *entrada=='o') return 1;
+    if (*entrada=='U' || *entrada=='u') return 1;
+    return 0;
+}
+
+int n_vogais (char * entrada)
+{
+    int i=0, n=0;
+
+    while (entrada[i]!='\0') {
+        if (ehvogal(&entrada[i])) n++;
+        i++;
+    }
+
+    return n;
+
+}
+
+int main ( ) 
+{
+
+    int i=0;
+    char entrada[255];
+
+    fgets(entrada, 255, stdin);
+
+    printf("%d\n", n_vogais(entrada));
+
+    return 0;
+  
+}
