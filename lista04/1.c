bb229e172415f8bfd34506f0dd55dfaed478e9e2
Marcos Suzuki <marcossuzuki@gmail.com>
2017-11-18 21:36:31 -0200

    Exercicio

--- lista04/1.c
+++ lista04/1.c
@@ -1,1 +1,51 @@
+/* lista 4 exercicio 1
+ * Crie uma struct para representar pontos em um espaco tridimencional (x,  y e z,  do
+ * tipo float).  Com essa struct crie uma funcao que receba dois pontos com coordenadas em
+ * ponto flutuante (no formato (x, y, z) e calcule a distancia entre dois pontos.  A saıda em
+ * ponto flutuante deve ser truncada em 2 casas decimais.  Para calcular a distancia entre
+ * dois pontos use a formula:
+ *
+ * Autor: Marcos Seiti Suzuki
+ * Data: 18/11/2017
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Vetor3 {
+    float x;
+    float y;
+    float z;
+} Vetor3;
+
+float distance (Vetor3 *a, Vetor3 *b) {
+
+    float d=0.0;
+
+    d+= pow((a->x - b->x),2);
+    d+= pow((a->y - b->y),2);
+    d+= pow((a->z - b->z),2);
+    d = pow(d,0.5);
+
+    return d;
+
+}
+
+int main () {
+
+    Vetor3 *a, *b;
+    a = malloc(sizeof(Vetor3));
+    b = malloc(sizeof(Vetor3));
+
+    scanf("%f, %f, %f", &(a->x), &(a->y), &(a->z));
+    scanf("%f, %f, %f", &(b->x), &(b->y), &(b->z));
+    
+    printf("%.2f\n", distance(a, b));
+    
+    free(a);
+    free(b);
+
+
+    return 0;
+}
--- lista04/2.c
+++ lista04/2.c
@@ -1,1 +1,70 @@
+/* lista 4 exercicio 2
+ * crie uma struct que represente um cronometro definido por minutos, segundos e deci-
+ * mos, todos inteiros. Faca uma funcao que calcule e retorne a diferenca entre duas medicoes
+ * de tempo (t2 - t1).  Utilize scanf(”%dm %ds %d”,...) como formatacao.
+ *
+ * Autor: Marcos Seiti Suzuki
+ * Data: 18/11/2017
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Cronometro {
+    unsigned int min, seg, dec;
+    char neg;
+} Cronometro;
+
+Cronometro diferenca (Cronometro *a, Cronometro *b) {
+
+    Cronometro d;
+    int decimal1=0, decimal2=0, result=0;
+
+    decimal1 += a->min*6000;
+    decimal1 += a->seg*100;
+    decimal1 += a->dec;
+    
+    decimal2 += b->min*6000;
+    decimal2 += b->seg*100;
+    decimal2 += b->dec;
+
+    result = decimal1 -  decimal2;
+    
+    d.neg='\0';
+    
+    if (result<0) {
+        d.neg='-';
+        result *= -1;
+    }
+        
+    d.dec = result%100;
+    result -= result%100;
+    result /= 100;
+    d.seg = result%60;
+    result -= result%60;
+    result /= 60;
+    d.min = result;
+    
+    return d;
+
+}
+
+int main () {
+
+    Cronometro *a, *b, dif;
+    a = malloc(sizeof(Cronometro));
+    b = malloc(sizeof(Cronometro));
+
+    scanf("%dm %ds %d", &(a->min), &(a->seg), &(a->dec));
+    scanf("%dm %ds %d", &(b->min), &(b->seg), &(b->dec));
+
+    dif = diferenca(a, b);
+    
+    printf("%c%dm %ds %d\n", dif.neg, dif.min, dif.seg, dif.dec);
+    
+    free(a);
+    free(b);
+
+    return 0;
+}
